8a59d4db00ea839af2091b4175297f0b95e65694
Uku Taht <uku.taht@gmail.com>
2014-12-19 16:13:59 +0000

    Adds filter for when skim is mentioned

--- lib/endpoint.rb
+++ lib/endpoint.rb
@@ -4,5 +4,17 @@ require 'skimbot/bot'
 
 post '/slack' do
   content_type :json 
-  {text:  Bot.new.response}.to_json
+  if skim_mentioned?
+    {text:  Bot.new.response}.to_json
+  else
+    response.status = 204
+  end
+end
+
+def skim_mentioned?
+  request_text.match /\bskim\b/
+end
+
+def request_text
+  JSON.parse(request.body.read)["text"]
 end
