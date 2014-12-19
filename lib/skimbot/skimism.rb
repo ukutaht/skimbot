b9937e1083543d4daac9d1bf428d87b2db89c45f
Uku Taht <uku.taht@gmail.com>
2014-12-19 16:21:55 +0000

    Updates frequencies

--- lib/skimbot/skimism.rb
+++ lib/skimbot/skimism.rb
@@ -1,19 +1,20 @@
 class Skimism
   PHRASES_WITH_FREQUENCIES = [
     ['this guy', 8],
+    ['got \'em', 7],
     ['wat', 6],
+    ['boom got \'em', 4],
     ['wat bro', 3],
+    ['whoa inappropiate', 3],
+    ['inappropriate', 3],
+    [':thisguy:', 3],
+    [':thisgal:', 3],
     ['watman', 2],
     ['what did you say to me?', 2],
     ['what for?', 2],
-    ['boom got \'em', 4],
-    ['whoa inappropiate', 3],
-    ['inappropriate', 3],
-    ['got \'em', 7],
-    [':thisguy:', 2],
-    [':thisgal:', 2],
-    [':skim:', 1],
-    [':boom:',1]]
+    [':boom: :goat: M', 2],
+    [':skim:', 2],
+    [':boom:',2]]
 
 
   PHRASES = PHRASES_WITH_FREQUENCIES.map(&:first)
